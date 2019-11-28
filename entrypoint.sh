ff0223ca320984731866b3b9d6421615f33139c8
Wizos <wizos@users.noreply.github.com>
2019-11-28 18:13:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 cd /brook
 wget -O brook https://github.com/txthinking/brook/releases/download/v${VERSION}/brook_linux_arm64
+# wget -O brook https://github.com/txthinking/brook/releases/download/v${VERSION}/brook
 
-cd /brook
-
+chmod +x /brook/brook
 chmod +x brook
 
 
