7a4414cdc81f78a09640f40cb4433f7a1e37eceb
Tommy <the3sky@gmail.com>
2020-01-01 21:14:31 +0800

    Update chmod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /brook
 wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${VERSION}/brook"
-chmod +x /brook/brook
+chmod +x brook
 
 ./brook --help
 echo "Hello,the world!"
